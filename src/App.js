088b7d5d0933d8667813b62714d128d40f13129e
Ovi-16 <147912055+Ovi-16@users.noreply.github.com>
2024-04-20 17:50:09 +0100

    cx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-// import LoginForm from './Components/LoginForm/Loginform.jsx';
-// import {Navbar} from './Components/NavBar/Navbar.jsx';
-// import {About} from './Components/LoginForm/About.jsx';
-// import {Register} from './Components/LoginForm/register.jsx';
-// import { Route, Routes } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
+import LoginForm from './Components/LoginForm/Loginform.jsx';
+import {Navbar} from './Components/NavBar/Navbar.jsx';
+import {About} from './Components/LoginForm/About.jsx';
+import {Register} from './Components/LoginForm/register.jsx';
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     // <React.Fragment>
@@ -15,13 +15,13 @@ function App() {
     //   </div>
     // </React.Fragment>
     <div className="App">
-      <header className="App-header">hello</header>
-      {/* <Navbar /> */}
-      {/* <Routes>
+      
+      <Navbar /> 
+      <Routes>
         <Route path="/" element={<About />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Register />} />
-      </Routes> */}
+      </Routes> 
     </div>
   );
 }
