f58d041d6c04af14fd86b72b990b5b53faedbd2f
Ovi-16 <147912055+Ovi-16@users.noreply.github.com>
2024-04-20 17:44:25 +0100

    vcx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import LoginForm from './Components/LoginForm/Loginform.jsx';
-import {Navbar} from './Components/NavBar/Navbar.jsx';
-import {About} from './Components/LoginForm/About.jsx';
-import {Register} from './Components/LoginForm/register.jsx';
-import { Route, Routes } from "react-router-dom";
+// import logo from './logo.svg';
+// import './App.css';
+// import LoginForm from './Components/LoginForm/Loginform.jsx';
+// import {Navbar} from './Components/NavBar/Navbar.jsx';
+// import {About} from './Components/LoginForm/About.jsx';
+// import {Register} from './Components/LoginForm/register.jsx';
+// import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     // <React.Fragment>
